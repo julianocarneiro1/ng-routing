9a5c1be04f3b7af93947822d22192d69ca0296c9
Juliano <julianoomc@gmail.com>
2021-10-13 16:49:30 -0300

    Populating and reading route parameters

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -12,6 +12,8 @@ import { RouterModule } from '@angular/router';
     SharedModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'products/:id/edit', component: ProductEditComponent },
     ])
   ],
   declarations: [
