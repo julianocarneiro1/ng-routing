96f7f427b51ce2d20745edd67845ce029d98b974
Juliano <julianoomc@gmail.com>
2021-10-15 10:37:24 -0300

    Implementing Error Handling to Resolver

--- src/app/products/product-resolver.service.ts
+++ src/app/products/product-resolver.service.ts
@@ -1,19 +1,35 @@
 import { Injectable } from '@angular/core'
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router'
-import { Observable } from 'rxjs'
+import { Observable, of } from 'rxjs'
+import { catchError, map } from 'rxjs/operators'
 
-import { Product } from './product'
+import { ProductResolved } from './product'
 import { ProductService } from './product.service'
 
 @Injectable({
     providedIn: 'root'
 })
-export class ProductResolver implements Resolve<Product> {
+export class ProductResolver implements Resolve<ProductResolved> {
 
     constructor(private productService: ProductService) { }
 
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Product> {
-        const id = route.paramMap.get('id')
+    resolve(route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<ProductResolved> {
+        const id = route.paramMap.get('id')        
+        if (isNaN(+id)) {
+            const message = `Product id was not a number: #{id}`
+            console.error(message)
+            return of({product: null, error: message})
+        }     
+        
         return this.productService.getProduct(+id)
+        .pipe(
+            map(product => ({product: product})),
+            catchError(error => {
+                const message = `Retrieval error: #{error}`
+                console.error(message)
+                return of({product: null, error: message})
+            })
+        )
     }
 }
