44b7197043127f59ea59f8f70bf2686a8f721634
Juliano <julianoomc@gmail.com>
2021-10-19 13:37:22 -0300

    Validating forms across Child Routes

--- src/app/products/product-edit/product-edit.component.ts
+++ src/app/products/product-edit/product-edit.component.ts
@@ -15,6 +15,7 @@ export class ProductEditComponent implements OnInit {
   errorMessage: string;
 
   product: Product;
+  private dataIsValid: { [key: string]: boolean } = {}
 
   constructor(
     private productService: ProductService,
@@ -58,6 +59,14 @@ export class ProductEditComponent implements OnInit {
     }
   }
 
+  isValid(path?: string): boolean {
+    this.validate()
+    if (path) {
+      return this.dataIsValid[path]
+    }
+    return (this.dataIsValid && Object.keys(this.dataIsValid).every(d => this.dataIsValid[d] === true))
+  }
+
   saveProduct(): void {
     if (true === true) {
       if (this.product.id === 0) {
@@ -83,5 +92,24 @@ export class ProductEditComponent implements OnInit {
 
     // Navigate back to the product list
     this.router.navigateByUrl("/products")
+  }
+
+  validate(): void {
+    //Clear the validation object
+    this.dataIsValid = {}
+
+    //'info' tab
+    if (this.product.productName && this.product.productName.length >= 3 && this.product.productCode) {
+          this.dataIsValid['info'] = true
+    } else {
+      this.dataIsValid['info'] = false
+    }
+
+    //'tags' tab
+    if (this.product.category && this.product.category.length >= 3) {
+      this.dataIsValid['tags'] = true
+    } else {
+      this.dataIsValid['tags'] = false
+    }
   }
 }
