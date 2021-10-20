1ee64fcce037f3afe740e36cecd4931cbf125758
Juliano <julianoomc@gmail.com>
2021-10-20 10:57:06 -0300

    Grouping routes using a component-less parent route

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -14,32 +14,30 @@ import { RouterModule } from '@angular/router';
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: 'products', component: ProductListComponent },
       {
-        path: 'products/:id',
-        component: ProductDetailComponent,
-        resolve: { resolvedData: ProductResolver }
-      },
-      {
-        path: 'products/:id/edit',
-        component: ProductEditComponent,
-        resolve: { resolvedData: ProductResolver },
+        path: 'products',        
         children: [
           {
             path: '',
-            redirectTo: 'info',
-            pathMatch: 'full'
+            component: ProductListComponent
           },
           {
-            path: 'info',
-            component: ProductEditInfoComponent
+            path: ':id',
+            component: ProductDetailComponent,
+            resolve: { resolvedData: ProductResolver }
           },
           {
-            path: 'tags',
-            component: ProductEditTagsComponent
+            path: ':id/edit',
+            component: ProductEditComponent,
+            resolve: { resolvedData: ProductResolver },
+            children: [
+              { path: '', redirectTo: 'info', pathMatch: 'full' },
+              { path: 'info', component: ProductEditInfoComponent },
+              { path: 'tags', component: ProductEditTagsComponent }
+            ]
           }
         ]
-      },
+      }
     ])
   ],
   declarations: [
