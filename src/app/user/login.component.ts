18d397a42e489f98433f704d4eaf9dc2e8374232
Juliano <julianoomc@gmail.com>
2021-10-22 14:30:24 -0300

    Sharing data with a Guard to remain on same route after login

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -12,7 +12,7 @@ export class LoginComponent {
   pageTitle = 'Log In';
 
   constructor(private authService: AuthService,
-              private router: Router) { }
+    private router: Router) { }
 
   login(loginForm: NgForm): void {
     if (loginForm && loginForm.valid) {
@@ -20,8 +20,12 @@ export class LoginComponent {
       const password = loginForm.form.value.password;
       this.authService.login(userName, password);
 
-      // Navigate to the Product List page after log in.
-      this.router.navigate(['/products'])
+      if (this.authService.redirectUrl) {
+        this.router.navigateByUrl(this.authService.redirectUrl)
+      } else {
+        this.router.navigate(['/products'])
+      }
+      
     } else {
       this.errorMessage = 'Please enter a user name and password.';
     }
